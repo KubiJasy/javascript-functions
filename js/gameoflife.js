941860fc6eb6645e28db25dce714571a538fc9cf
Jasmine Ampofo <akubahjazz@gmail.com>
2021-10-07 09:39:44 +0000

    2.0

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -8,19 +8,95 @@ function same([x, y], [j, k]) {
     return true;
   }
   return false;
-}
+};
 
 // The game state to search for `cell` is passed as the `this` value of the function.
 function contains(cell) {
-  // let argIndex = cell.length;
-  this.includes(cell);
-}
+  let counter = 0;
+  for (i = 0; i < this.length; i++) {
+    if(same(this[i], cell)){
+      counter++;
+    }
+    }
+    return counter;
+  };
 
-const printCell = (cell, state) => {};
 
-const corners = (state = []) => {};
+const printCell = (cell, state) => {
+  //uses the contain function but the game state passes which is the 'this' keyword
+  //is with respect to the argument passed for the game state in the printCell func
+  //Any other arguments needed for the function are passed after this
+  if (contains.call(state, cell)){
+    return unescape('\u25A3');
+  }else {
+    return unescape('\u25A2');
+  }
+};
+const corners = (state = []) => {
+  if (state.length === 0){
+    return {topRight: [0,0],
+    bottomLeft: [0,0]};
+  }else {
+    var maxXCoord = state[0][0];
+    var maxYCoord = state[0][1];
+    var minXCoord = state[0][0];
+    var minYCoord = state[0][1];
+    var minCellIdx = 0
+    var maxCellIdx = 0
+  }
 
-const printCells = (state) => {};
+  for (let i = 0; i < state.length; i++) {
+    if (state[i][0] >= maxXCoord){
+        maxXCoord = state[i][0];
+        maxCellIdx = i;
+    }else if (state[i][0] <= minXCoord){
+      minXCoord = state[i][0];
+      minCellIdx = i;
+  }
+    if (state[i][1] >= maxYCoord) {
+        maxYCoord = state[i][1];
+    }else if (state[i][1] <= minYCoord) {
+        minYCoord = state[i][1];
+    }
+    }
+
+
+  var newMinYCoord = minYCoord
+  do {
+    minYCoord++;
+  } while (minYCoord < maxYCoord);
+
+  do {
+    maxYCoord--;
+  } while (maxYCoord > newMinYCoord);
+
+  let topRight = [state[maxCellIdx][0], minYCoord];
+  let bottomLeft = [state[minCellIdx][0], maxYCoord];
+
+  return {topRight: topRight,
+  bottomLeft: bottomLeft
+  };
+};
+
+const printCells = (state) => {
+  var cellRep = "";
+  var corners1 = corners(state);
+  var a = corners1.topRight[0];
+  var b = corners1.bottomLeft[1];
+  var cellXCoord = corners1.bottomLeft[0];
+  var cellYCoord = corners1.topRight[1];
+
+  while (!(same([cellXCoord, cellYCoord], [a,b]))) {
+    while (cellYCoord < (a+1)) {
+      cellRep = printCell([cellXCoord, cellYCoord], state) + " ";
+      cellYCoord++;
+    }
+    cellYCoord--;
+    cellRep.concat("\n")
+    continue;
+  }
+  return cellRep;
+};
 
 const getNeighborsOf = ([x, y]) => {};
 
